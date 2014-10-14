4906a704f1208817d49bd993a3a80caf34580093
Constantin Gavrilete <c.gavrilete@gmail.com>
2014-10-15 00:57:15 +0200

    testing and updating multiple words

--- app/scripts/50vad.js
+++ app/scripts/50vad.js
@@ -12,14 +12,60 @@
 		function ($scope, MessageService) {
 			$scope.vocable = {};
 			$scope.redirectUrl = '';
+			$scope.vocable = {};
+			$scope.translation = '';
+			$scope.displayedChars = 0;
+
+			$scope.isCorrect = false;
+			$scope.isIncorrect = false;
 
 			$scope.init = function () {
 				MessageService.registerReceiver($scope.onMessage);
+
+				MessageService.sendMessage({
+					type: 'GET-NEXT-VOCABLE'
+				}, function (nextVocable) {
+					$scope.vocable = nextVocable;
+					$scope.$apply();
+				});
+			};
+
+			$scope.checkTranslation = function () {
+				if ($scope.translation === $scope.vocable.t) {
+					$scope.isCorrect = true;
+					$scope.isIncorrect = false;
+					$scope.vocable.l++;
+				} else {
+					$scope.isIncorrect = true;
+					$scope.vocable.l = 1;
+				}
+
+				MessageService.sendMessage({
+					type: 'UPDATE-VOCABLE',
+					vocable: $scope.vocable
+				}, function () {
+					window.setTimeout(function (hasNext) {
+						if (hasNext) {
+							// load new vocable
+							document.location.href = document.location.href;
+						} else {
+							// redirect to previous page
+							window.history.back();
+						}
+					}, 1500);
+				});
 			};
 
 			$scope.onMessage = function (message) {
 				if (message.type === 'REDIRECT-URL') {
 					$scope.redirectUrl = message.redirectUrl;
+				}
+			};
+
+			$scope.showHint = function () {
+				if ($scope.displayedChars < $scope.vocable.t.length) {
+					$scope.displayedChars++;
+					$scope.translation = $scope.vocable.t.substr(0, $scope.displayedChars);
 				}
 			};
 
--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,7 +1,15 @@
 'use strict';
 
 var shutupUntil = Date.now(),
-	vocableManager = new window.VocableManager('en', 'de');
+	vocableManager = new window.VocableManager('en', 'de'),
+	levels = {
+		1: 600 * 1000,
+		2: 3600 * 1000,
+		3: 5 * 3600 * 1000,
+		4: 24 * 3600 * 1000,
+		5: 25 * 24 * 3600 * 1000,
+		6: 120 * 24 * 3600 * 1000
+	};
 
 // ------------------------------------
 // Helper functions
@@ -46,6 +54,84 @@ var updateShutupUntil = function () {
 	});
 };
 
+var storeVocable = function (vocable, translation) {
+	var record = {};
+
+	// storing selected vocable
+	record['_' + vocable] = {
+		v: vocable,
+		t: translation,
+		s: '... no sentence yet ...',
+		l: 1,
+		ts: Date.now() + 600 * 1000
+	};
+	chrome.storage.local.set(record);
+
+	// storing timestamp when vocable should be checked next
+	chrome.storage.local.get('next', function (nextRecord) {
+		if (!nextRecord.next) {
+			nextRecord.next = {};
+		}
+
+		// next check in 10 minutes
+		nextRecord.next[record['_' + vocable].ts] = '_' + vocable;
+		chrome.storage.local.set(nextRecord);
+	});
+};
+
+var getNextVocable = function (callback) {
+	chrome.storage.local.get('next', function (nextRecord) {
+		var keys,
+			nextKey,
+			nextVocable;
+
+		if (nextRecord.next) {
+			keys = Object.keys(nextRecord.next);
+			nextKey = keys.shift();
+			if (nextKey) {
+				nextVocable = nextRecord.next[nextKey];
+				chrome.storage.local.get(nextVocable, function (nextVocableObject) {
+					return callback(nextVocableObject[nextVocable]);
+				});
+			} else {
+				return callback(null);
+			}
+		} else {
+			return callback(null);
+		}
+	});
+};
+
+var updateVocable = function (vocableObject, callback) {
+	var recordToStore = {},
+		oldTimestamp = vocableObject.ts;
+
+	// check whether level increased
+	if (vocableObject.l === 1) {
+		// user did wrong
+		getNextVocable(function (nextVocableObject) {
+			// TODO: update next!
+			return callback(!!nextVocableObject);
+		});
+	} else {
+		// updating vocable record
+		vocableObject.ts = Date.now() + levels[vocableObject.l];
+		recordToStore['_' + vocableObject.v] = vocableObject;
+		chrome.storage.local.set(recordToStore, function () {
+			// updating next record
+			chrome.storage.local.get('next', function (nextRecord) {
+				if (!nextRecord.next) {
+					nextRecord.next = {};
+				}
+
+				delete nextRecord.next[oldTimestamp];
+				nextRecord.next[vocableObject.ts] = '_' + vocableObject.v;
+				chrome.storage.local.set(nextRecord, callback);
+			});
+		});
+	}
+};
+
 // ------------------------------------
 // Event listener
 // ------------------------------------
@@ -56,15 +142,17 @@ chrome.runtime.onInstalled.addListener(function (details) {
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 	if (message.type === 'SHUT-UP') {
 		shutup(message.duration);
-	} else if (message.type === 'GET-TRANSLATION') {
-		vocableManager.getTranslation(message.vocable, function (err, translation) {
-			sendResponse({
-				err: err,
-				translation: translation
-			});
+	} else if (message.type === 'GET-NEXT-VOCABLE') {
+		getNextVocable(function (nextVocableObject) {
+			console.log(nextVocableObject);
+			sendResponse(nextVocableObject);
+		});
+	} else if (message.type === 'UPDATE-VOCABLE') {
+		updateVocable(message.vocable, function (hasNext) {
+			sendResponse(hasNext);
 		});
 	}
-	
+
 	return true;
 });
 
@@ -72,7 +160,9 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
 	if (changeInfo.status === 'loading' && changeInfo.url) {
 		console.log('Current url: ' + changeInfo.url + ', shutting up for ' + ((shutupUntil - Date.now()) / 1000) + ' seconds.');
 
-		if ((shutupUntil - Date.now()) < 0) {
+		// if ((shutupUntil - Date.now()) < 0) {
+		if (changeInfo.url.indexOf('chrome-extension://') === -1) {
+		// if (false) {
 			chrome.tabs.update(tabId, {
 				url: '/50vad.html'
 			}, function () {
@@ -86,3 +176,22 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
 // Entry point
 // ------------------------------------
 updateShutupUntil();
+
+// creating context menus
+chrome.contextMenus.create({
+	type: 'normal',
+	title: 'Translate with 50VocabsADay',
+	contexts: ['selection'],
+	onclick: function (info) {
+		var vocable = info.selectionText;
+
+		vocableManager.getTranslation(vocable, function (err, translation) {
+			if (err) {
+				console.log(err);
+				return;
+			}
+
+			storeVocable(vocable, translation);
+		});
+	}
+});

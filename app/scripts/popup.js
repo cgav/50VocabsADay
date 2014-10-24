9f485e72e0fb22aea4a8e3b07e4560415426dae8
Constantin Gavrilete <c.gavrilete@gmail.com>
2014-10-24 23:27:31 +0200

    changing vocable which should be added to vocabulary list

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -13,6 +13,10 @@
 			$scope.q = '';
 			$scope.result = {};
 			$scope.entries = [];
+			$scope.translation = {
+				v: '',
+				t: ''
+			};
 
 			$scope.init = function () {
 				// connecting to background page
@@ -26,20 +30,34 @@
 					fromLang: 'en',
 					toLang: 'de'
 				}, function (result) {
+					var baseForm = 'base_form';
+
 					if (result.error) {
 						return window.alert(result.error);
 					}
 
-					$scope.result = result.translationResult;
-
-					if ($scope.result.dict && $scope.result.dict.length > 0) {
-						$scope.entries = $scope.result.dict[0].entry;
+					// preparsing result entries
+					if (result.translationResult.dict && result.translationResult.dict.length > 0) {
+						result.translationResult.dict.forEach(function (dict) {
+							dict.entry.forEach(function (entry) {
+								$scope.entries.push(entry.word);
+							});
+						});
+						$scope.translation.v = result.translationResult.dict[0][baseForm];
+					} else if (result.translationResult.sentences && result.translationResult.sentences.length > 0) {
+						$scope.entries.push(result.translationResult.sentences[0].trans);
+						$scope.translation.v = result.translationResult.sentences[0].orig;
 					}
+					$scope.translation.t = $scope.entries[0];
+					$scope.changeTranslation($scope.translation.v, $scope.translation.t);
 					$scope.$apply();
 				});
 			};
 
 			$scope.changeTranslation = function (v, t) {
+				$scope.translation.t = t;
+				$scope.$apply();
+
 				MessageService.sendMessage({
 					type: 'CHANGE-POPUP-TRANSLATION',
 					v: v,
@@ -47,12 +65,8 @@
 				});
 			};
 
-			$scope.showDictTranslation = function () {
-				return $scope.result && $scope.result.dict;
-			};
-
-			$scope.showSentenceTranslation = function () {
-				return $scope.result && !$scope.result.dict && $scope.result.sentences;
+			$scope.showTranslation = function () {
+				return $scope.entries.length > 0;
 			};
 
 			$scope.isReturn = function (event) {
